4fcc871bc52f665458efb372769148ec483af324
Rogelio <rogercp1234@yahoo.com>
2020-03-09 14:26:59 -0700

    initial commit

---
+++ webmud/src/main/java/com/rogercp/webmud/WebmudApplication.java
@@ -0,0 +1,15 @@
+package com.rogercp.webmud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmudApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WebmudApplication.class, args);
+    }
+
+}
---
+++ webmud/src/test/java/com/rogercp/webmud/WebmudApplicationTests.java
@@ -0,0 +1,15 @@
+package com.rogercp.webmud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmudApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
